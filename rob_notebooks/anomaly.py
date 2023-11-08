bde26c3b31a3234bbb7ad6252ed47c9d9a8a729e
Rob Fatland <rob5@uw.edu>
2023-11-08 06:30:09 -0800

    aha

--- rob_notebooks/anomaly.py
+++ rob_notebooks/anomaly.py
@@ -1,11 +1,9 @@
 import os, sys, time, glob, warnings
-from os.path import join as joindir
-from IPython.display import clear_output
 from matplotlib import pyplot as plt
 from matplotlib import colors as mplcolors
+from matplotlib.pyplot import figure
 import numpy as np, pandas as pd, xarray as xr
 from numpy import datetime64 as dt64, timedelta64 as td64
-
 from math import fabs, sqrt
 
 
